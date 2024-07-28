e69e73102741dea8dda919adb4a44885dd8aabc8
Felipe Duarte <felipedealmeidaduarte@gmail.com>
2024-07-28 09:49:21 -0300

    Criando a pasta ESTUDO SQL
    Criação da pasta Estudo SQL e aprendendo a fazer a criação de um banco de dados.

---
+++ Estudo SQL/CriandoBancoDeDados.sql
@@ -0,0 +1,16 @@
+/*
+	Criando um Banco de Dados
+	Autor: Felipe de Almeida Duarte
+	Data: 28/07/2024
+*/
+
+-- Criando uma Banco via T-SQL
+CREATE DATABASE DepartamentoPessoal;
+
+-- Colocando um Banco em Uso
+USE DepartamentoPessoal;
+
+-- Excluir um Banco de Dados
+-- O mesmo não poderá estar em uso
+USE master;
+DROP DATABASE DepartamentoPessoal;
