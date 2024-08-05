c8acf9e21596d5067717c440a668a07fd1c787c2
Felipe Duarte <felipedealmeidaduarte@gmail.com>
2024-08-05 08:22:25 -0300

    Estudando o T-SQL
    Hoje eu aprendi sobre o T-SQL, criei um banco de dados e vi um pouco sobre o CRUD.

---
+++ Curso SQL Server/Curso SQL Server/Curso SQL Server/Criando um Banco de Dados.sql
@@ -0,0 +1,16 @@
+/*
+	Criando um Banco dee Dados
+	Autor: Felipe de Almeida Duarte
+	Data: 05/08/2023
+*/
+
+-- Criando um Banco via T-SQL
+CREATE DATABASE DepartamentoPessoal;
+
+-- Colocando um Banco em Uso
+USE DepartamentoPessoal;
+
+-- Excluir um Banco de Dados
+-- O mesmo não poderá estar em uso.
+USE master;
+DROP DATABASE DepartamentoPessoal;
